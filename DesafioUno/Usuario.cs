bd136b8b227e40aa83e6c63cbaf2fd51f6ec6c4a
NinaSqrl <117684492+NinaSqrl@users.noreply.github.com>
2022-11-07 17:26:42 -0300

    Add files via upload

---
+++ DesafioUno/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioUno
+{
+    public class Usuario
+    {
+        private int Id;
+        private string Nombre;
+        private string Apellido;
+        private string NombreUsuario;
+        private string Contrasena;
+        private string Mail;
+    }
+}
+
---
+++ DesafioUno/Venta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioUno
+{
+    public class Venta
+    {
+        private int Id;
+        private string Comentarios;
+    }
+}
