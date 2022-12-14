ee6d68977f2a39b3974b51003251bd0b83a71890
NinaSqrl <117684492+NinaSqrl@users.noreply.github.com>
2022-12-14 16:22:35 -0300

    Add files via upload

---
+++ ProyectofinalCoradini/ProyectofinalCoradini/ADO.NET/ProyectofinalCoradini/ADO.NET/UsuarioHandler.cs
@@ -0,0 +1,78 @@
+﻿using ProyectofinalCoradini.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ProyectofinalCoradini.ADO.NET
+{
+    public class UsuarioHandler
+    {
+        private SqlConnection Conexion;
+        private string StringConexion =
+            "Server=sql.bsite.net\\MSSQL2016;" +
+            "Database=ninasqrl_ProyectofinalCoradini;" +
+            "User Id=ninasqrl_ProyectofinalCoradini;" +
+            "Password=Cleo__24;";
+        public UsuarioHandler() 
+        {
+            try
+            {
+                Conexion = new SqlConnection(StringConexion);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
+        // acá CargarUsuario()
+
+        private Usuario CargarUsuario(SqlDataReader reader)
+        {
+            Usuario usuario = new Usuario();
+            usuario.Id = Convert.ToInt32(reader["Id"].ToString());
+            usuario.Nombre = reader["Nombre"].ToString();
+            usuario.Apellido = reader["Apellido"].ToString();
+            usuario.NombreUsuario = reader["NombreUsuario"].ToString();
+            usuario.Contrasenia = reader["Contrasenia"].ToString();
+            usuario.Mail = reader["Mail"].ToString();
+            return usuario;
+        }
+
+        public List<Usuario> GetUsuario() 
+        {
+            List<Usuario> listaUsuarios = new List<Usuario>();
+            if (StringConexion == null)
+            {
+                throw new Exception("Conexión no realizada");
+            }
+            try
+            {
+                using (SqlCommand command = new SqlCommand ("SELECT * FROM Usuario", Conexion))
+                {
+                    Conexion.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                Usuario usuario = CargarUsuario(reader);
+                                listaUsuarios.Add(usuario);
+                            }
+                        }
+                    }
+                }
+                Conexion.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return listaUsuarios;
+        }
+    }
+
+}
---
+++ ProyectofinalCoradini/ProyectofinalCoradini/ADO.NET/ProyectofinalCoradini/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectofinalCoradini.ADO.NET;
+using ProyectofinalCoradini.Models;
+// using ProyectofinalCoradini.Repositories;
+using System.Data.SqlClient;
+
+namespace ProyectofinalCoradini.Controllers
+{
+    [ApiController]
+    [Route("Api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private LoginHandler handler = new LoginHandler();
+
+        [HttpPost]
+        public ActionResult<Usuario> Login(Usuario usuario)
+        {
+            try
+            {
+                bool usuarioExiste = handler.LoginUsuario(usuario);
+                return usuarioExiste ? Ok() : NotFound();
+            }
+            catch (Exception ex)
+            {
+
+                return Problem(ex.Message);
+            }
+        }
+    }
+}
